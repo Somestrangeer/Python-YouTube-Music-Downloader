107e2833962f8351ffe1051986ced4e92778ca35
Alexander <95109481+Somestrangeer@users.noreply.github.com>
2022-05-16 20:20:43 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,42 +6,58 @@ import pytube
 
 
 def some_func(message, text, yt_var):
-    global name #make it global to schow a title to a user
-    global val #make it global to close or finish the loop
+    global name
+    global val
+    global path
     try:
         yt = yt_var
-        name = yt.title #We get a video's name
-        name_up = re.sub("[$|@|&|.|:|!|#|№|^|?|*|=|+|>|<|/|;|~|`|']", "", name) #We make the title more correct
-
-        #We download a video
-
-        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download()
+        name = yt.title
+        name_up = re.sub("[$|@|&|.|:|!|#|№|^|?|*|=|+|>|<|/|;|~|`|'|ﾉ]", "", name)
+        path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download()
     except:
-
-        #If we get an error, we close the loop and redirect to other function
-
         val = False
         error_message(message)
-    def convert_video_to_audio_ffmpeg(video_file, name_file, output_ext="mp3"):
-        try:
-            ext = os.path.splitext(video_file)
-            subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{name_file}.{output_ext}"],
-                            stdout=subprocess.DEVNULL,
-                            stderr=subprocess.STDOUT)
-        except:
-            val = False
-            error_message(message)
-
-    vf = f'{name_up}.mp4'
-    convert_video_to_audio_ffmpeg(vf, name_up)
+        
+        
+    def convert_video_to_audio_ffmpeg(video_file, name_file, prevent_error):
+        ext = os.path.splitext(video_file)
+        a = subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{name_file}.mp3"],
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
+        
+        if a == 1:
+            b = subprocess.call(["ffmpeg", "-y", "-i", prevent_error, f"{name_file}.mp3"],
+                                stdout=subprocess.DEVNULL,
+                                stderr=subprocess.STDOUT)
+            
+            if b == 1:
+                subprocess.call(["ffmpeg", "-y", "-i", path, f"{name_file}.mp3"],
+                                stdout=subprocess.DEVNULL,
+                                stderr=subprocess.STDOUT)
+    vf = f"{path}"
+    prev_er_vf = f"{path}"
+    convert_video_to_audio_ffmpeg(vf, name_up, prev_er_vf)
+    
     try:
         file_name_audio = f"{name_up}.mp3"
-        audio = open(r'{0}'.format(file_name_audio), 'rb') #open converted file
-        val = False    #finish the loop
-        bot.send_audio(message.chat.id, audio) 
+        audio = open(r'{0}'.format(file_name_audio), 'rb')
+        bot.send_audio(message.chat.id, audio)
         audio.close()
-        os.remove(vf)
-        os.remove(file_name_audio)
+        val = False
     except:
         val = False
         error_message(message)
+        
+    try:
+        os.remove(vf)
+        val = False
+    except:
+        pass
+    
+    try:
+        os.remove(prev_er_vf)
+        val = False
+    except:
+        pass
+    os.remove(file_name_audio)
+    val = False
