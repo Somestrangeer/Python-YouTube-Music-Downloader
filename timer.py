dcb60071872e183f4d04bc922c0ac1d6d9f3411d
Alexander <95109481+Somestrangeer@users.noreply.github.com>
2023-07-11 20:32:18 +0300

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,15 +1,12 @@
-import telebot
-import time
-
-
-def timer(message, a):
-    global val   #to control the finction
-    val = True
-    link = a
-    i=0
-    while val == True:
-        time.sleep(1)
-        i+=1
-        bot.edit_message_text(chat_id=message.chat.id, message_id=a.message_id, text=f"Прошло {i} секунд. \n"
-                                                                                     f"\n"
-                                                                                     f"Скачиваю - {name}")
+class timer:
+	def start(self, message, waitMsg, name, boolTimer):
+		seconds = 0
+		self.name = name
+		self.boolTimer = boolTimer
+		while self.boolTimer.getBoolTimer() == True:
+			time.sleep(1)
+			seconds += 1
+			bot.edit_message_text(chat_id=message.chat.id, message_id=waitMsg.message_id, text=f"Прошло {seconds} секунд. \n"
+																							f"\n"
+																							f"Скачиваю - {self.name.getName()}")
+		bot.delete_message(chat_id=message.chat.id, message_id=waitMsg.message_id)
